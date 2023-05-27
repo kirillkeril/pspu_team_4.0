1556adf44bf6ff11e85181633a594cc6314d13a9
kirillkeril <89710374+kirillkeril@users.noreply.github.com>
2023-05-27 15:44:28 +0500

    name

--- dto/user.dto.js
+++ dto/user.dto.js
@@ -1,11 +1,13 @@
 export default class UserDto {
     email;
+    name;
     id;
     isActivated;
     roles;
 
     constructor(model) {
         this.email = model.email;
+        this.email = model.name;
         this.id = model.id;
         this.isActivated = model.isActivated;
         this.roles = model.roles;
